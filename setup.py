dfc1330b2c3e7bd10fb27af947a6ea13fdbe2699
Ritowsky <wojtek020320@o2.pl>
2025-04-29 12:17:05 +0200

    Add initial implementation for STServo SDK Python library
    This commit introduces the foundational structure and implementation for the STServo SDK. It includes modules for handling STS/SCSCL protocol communication, port management, and synchronized operations such as GroupSyncRead/Write. The code defines key packet-level communication concepts and servo controls.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='STservo_sdk',
+    version='0.1.0',
+    description='Python SDK do sterowania serwami ST',
+    author='ktoś',
+    packages=find_packages(),  # automatycznie znajdzie STservo_sdk
+    install_requires=[],
+)
